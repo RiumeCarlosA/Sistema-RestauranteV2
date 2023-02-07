5aaf359a8b625dfb61037bd0fddaca0afb657ad9
RiumeCarlosA <riumecarlos@hotmail.com>
2023-02-07 21:39:07 +0000

    commit inicial

---
+++ src/main/java/br/com/riume/restaurante/SistemaRestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.riume.restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/riume/restaurante/enums/Perfil.java
@@ -0,0 +1,32 @@
+package br.com.riume.restaurante.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Perfil {
+	
+	ADMIN(0, "ROLE_ADMIN"), CAIXA(1, "ROLE_CAIXA"), ATENDENTE(2, "ROLE_ATENDENTE");
+	
+	private Integer codigo;
+	
+	private String descricao;
+	
+	private Perfil(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	
+	public static Perfil toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(Perfil x : Perfil.values()) {
+			if(cod.equals(x.getCodigo())){
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Perfil inválido");
+	}
+}
