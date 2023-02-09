0d398c66e94b134f7f0e4fa2cbea37434a0213d6
RiumeCarlosA <riumecarlos@hotmail.com>
2023-02-09 02:58:39 +0000

    Organizando models

--- src/main/java/br/com/riume/restaurante/controller/AtendenteController.java
+++ src/main/java/br/com/riume/restaurante/controller/AtendenteController.java
@@ -1,5 +1,32 @@
 package br.com.riume.restaurante.controller;
 
-public class AtendenteController {
+import java.net.URI;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.riume.restaurante.dto.AtendenteDTO;
+import br.com.riume.restaurante.model.usuarios.Atendente;
+import br.com.riume.restaurante.service.AtendenteService;
+
+@RestController
+@RequestMapping(value = "/atendente")
+public class AtendenteController {
+	
+	@Autowired
+	private AtendenteService service;
+	
+	@PostMapping("")
+	public ResponseEntity<AtendenteDTO> create(@Valid @RequestBody AtendenteDTO objDTO){
+		Atendente newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
--- src/main/java/br/com/riume/restaurante/service/config/DBService.java
+++ src/main/java/br/com/riume/restaurante/service/config/DBService.java
@@ -1,11 +1,19 @@
 package br.com.riume.restaurante.service.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.riume.restaurante.model.usuarios.Atendente;
+import br.com.riume.restaurante.repository.PessoaRepository;
 
 @Service
 public class DBService {
 		
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
 	public void instanciaDB() {
-		
+		Atendente tec1 = new Atendente("Riume Carlos", "140.946.719-86", "riumecarlos@gmail.com", "123@mudar");
+		pessoaRepository.save(tec1);
 	}
 }
